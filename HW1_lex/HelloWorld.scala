c093ae0a246af1170a6c3b93edcdb1d0fe6a2fae
Teddy <mickey23405383@gmail.com>
2020-05-16 15:55:05 +0800

    makeFile

--- HW1_lex/HelloWorld.scala
+++ HW1_lex/HelloWorld.scala
@@ -1,12 +1,7 @@
 /* Hello World Example */
 object HelloWorld {
   def main ( ) {
-  // Print text to the console
-  println ("aa""bb");
-  boolean a = false
-  var b = 10
-  if a == true {
-    println("T");
-  }
+    // Print text to the console
+    println ("aa""bb");
 	} 
 }
